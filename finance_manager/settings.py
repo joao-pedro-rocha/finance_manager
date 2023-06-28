3e556c985d042e59b3b4d1fbca4d22815f15ad72
joao pedro <joaorocha.ar@gmail.com>
2023-06-28 17:05:51 -0300

    bugfix: dates

--- finance_manager/settings.py
+++ finance_manager/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,6 +128,9 @@ STATIC_URL = 'static/'
 STATIC_ROOT = config('STATIC_ROOT')
 MEDIA_URL = 'media/'
 MEDIA_ROOT = config('MEDIA_ROOT')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
